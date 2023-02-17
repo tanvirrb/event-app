edd9fb302b2a93c2e36414e6612d567fc60e95a5
Tanvir Islam <tanvir.rb@outlook.com>
2023-02-17 20:31:27 +0600

    refactor: services refactored for better performance

--- src/events/events.service.js
+++ src/events/events.service.js
@@ -1,17 +1,12 @@
 const { Event } = require('../models');
-const _p = require('../helpers/asyncWrapper');
 
 module.exports = {
   createEvent: async (eventData) => {
-    const [err, newEvent] = await _p(Event.create(eventData));
-    if (err) console.error('Service createEvent err', err);
-    return newEvent;
+    return Event.create(eventData);
   },
 
   getEventById: async (id) => {
-    const [err, event] = await _p(Event.findOne({ where: { id } }));
-    if (err) console.error('Service getEventById err', err);
-    return event;
+    return Event.findByPk(id, { raw: true });
   },
 
   deleteEvents: async () => {
