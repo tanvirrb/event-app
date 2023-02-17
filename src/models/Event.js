0a7d5f6d536f70a617466be9e1f1d223aacd40c9
Tanvir Islam <tanvir.rb@outlook.com>
2023-02-17 22:04:50 +0600

    refactor: unnecessary comment removed

--- src/models/Event.js
+++ src/models/Event.js
@@ -2,9 +2,7 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
   Event.init(
     {
