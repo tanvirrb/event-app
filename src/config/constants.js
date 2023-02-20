80db82cacbe886445e2af25bcfc904f23e5547f4
Tanvir Islam <tanvir.rb@outlook.com>
2023-02-20 13:01:38 +0600

    refactor: http status constants added

---
+++ src/config/constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+  HTTP_INTERNAL_SERVER_ERROR: {
+    code: 500,
+    message: 'Internal server error',
+  },
+  HTTP_NOT_FOUND: {
+    code: 404,
+    message: 'not found',
+  },
+  HTTP_CREATED: {
+    code: 201,
+    message: 'successfully created',
+  },
+  HTTP_OK: {
+    code: 200,
+    message: 'successfully fetched',
+  },
+  HTTP_BAD_REQUEST: {
+    code: 400,
+    message: 'bad request',
+  },
+};
--- src/events/events.controller.js
+++ src/events/events.controller.js
@@ -1,61 +1,70 @@
 const eventsService = require('./events.service');
+const {
+  HTTP_NOT_FOUND,
+  HTTP_CREATED,
+  HTTP_OK,
+} = require('../config/constants');
 
-module.exports.createEvent = async (req, res) => {
+module.exports.createEvent = async (req, res, next) => {
   try {
     const event = await eventsService.createEvent(req.body);
-    res.status(201).json({ data: event });
+    return res.status(HTTP_CREATED.code).json({ data: event });
   } catch (err) {
     console.error('Controller createEvent err', err);
-    res.status(500).json({ error: 'Internal server error' });
+    return next(new Error(err));
   }
 };
 
-module.exports.getEvents = async (req, res) => {
+module.exports.getEvents = async (req, res, next) => {
   try {
     const { pageNumber, pageSize } = req.query;
     const [events, totalEvents] = await Promise.allSettled([
       eventsService.getEvents(pageNumber, pageSize),
       eventsService.getEventCount(),
     ]);
-    res
-      .status(200)
+
+    return res
+      .status(HTTP_OK.code)
       .json({ pageNumber, pageSize, totalEvents: totalEvents.value, data: events.value });
   } catch (err) {
     console.error('Controller getEvents err', err);
-    res.status(500).json({ error: 'Internal server error' });
+    return next(new Error(err));
   }
 };
 
-module.exports.getEventById = async (req, res) => {
+module.exports.getEventById = async (req, res, next) => {
   try {
     const event = await eventsService.getEventById(req.params.id);
-    if (!event) return res.status(404).json({ error: 'Event not found' });
-    res.status(200).json({ data: event });
+    if (!event) return res.status(HTTP_NOT_FOUND).json({ error: HTTP_NOT_FOUND.message });
+
+    return res.status(HTTP_OK.code).json({ data: event });
   } catch (err) {
     console.error('Controller getEventById err', err);
-    res.status(500).json({ error: 'Internal server error' });
+    return next(new Error(err));
   }
 };
 
-module.exports.updateEventById = async (req, res) => {
+module.exports.updateEventById = async (req, res, next) => {
   try {
     const event = await eventsService.updateEventById(req.params.id, req.body);
-    if (!event) return res.status(404).json({ error: 'Event not found' });
+    if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
     const updatedEvent = await eventsService.getEventById(req.params.id);
-    res.status(200).json({ data: updatedEvent });
+
+    return res.status(HTTP_OK.code).json({ data: updatedEvent });
   } catch (err) {
     console.error('Controller updateEventById err', err);
-    res.status(500).json({ error: 'Internal server error' });
+    return next(new Error(err));
   }
 };
 
-module.exports.deleteEventById = async (req, res) => {
+module.exports.deleteEventById = async (req, res, next) => {
   try {
     const event = await eventsService.deleteEventById(req.params.id);
-    if (!event) return res.status(404).json({ error: 'Event not found' });
-    res.status(200).json({ data: !!event });
+    if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
+
+    return res.status(HTTP_OK.code).json({ data: !!event });
   } catch (err) {
     console.error('Controller deleteEventById err', err);
-    res.status(500).json({ error: 'Internal server error' });
+    return next(new Error(err));
   }
 };
