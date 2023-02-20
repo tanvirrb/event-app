96384450c81d551c73f99130b60ef5b1a87e6e22
Tanvir Islam <tanvir.rb@outlook.com>
2023-02-20 15:12:14 +0600

    feat: request validator added for get all events

--- src/events/events.validator.js
+++ src/events/events.validator.js
@@ -1,6 +1,19 @@
 const Joi = require('joi');
 const validationMessage = require('../helpers/validationMessageFormatter');
 
+module.exports.getEventsValidator = (req, res, next) => {
+    const schema = Joi.object().options({ abortEarly: false, stripUnknown: true }).keys({
+      pageNumber: Joi.number().required().integer().min(1).default(1),
+        pageSize: Joi.number().required().integer().min(1).default(5),
+    });
+
+    const { error } = schema.validate(req.query);
+    if (error) {
+        const message = validationMessage(error?.details);
+        return res.status(400).json({ error: message });
+    }
+    next();
+};
 module.exports.createEventValidator = (req, res, next) => {
   const schema = Joi.object().options({ abortEarly: false, stripUnknown: true }).keys({
     name: Joi.string().required(),
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const eventsController = require('../events/events.controller');
-const { createEventValidator, updateEventValidator } = require('../events/events.validator');
+const { createEventValidator, updateEventValidator, getEventsValidator} = require('../events/events.validator');
 const withErrorHandler = require('../helpers/controllerErrorHandler');
 
 router.post('/', createEventValidator, withErrorHandler(eventsController.createEvent));
-router.get('/', withErrorHandler(eventsController.getEvents));
+router.get('/',getEventsValidator, withErrorHandler(eventsController.getEvents));
 router.get('/:id', withErrorHandler(eventsController.getEventById));
 router.put('/:id', updateEventValidator, withErrorHandler(eventsController.updateEventById));
 router.delete('/:id', withErrorHandler(eventsController.deleteEventById));
