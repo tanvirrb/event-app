41ea4b012ffff3c236958b30f60a65afada7dfa1
Tanvir Islam <tanvir.rb@outlook.com>
2023-02-20 13:54:01 +0600

    refactor: controller refactored to reduce boilerplate code

--- src/events/events.controller.js
+++ src/events/events.controller.js
@@ -1,70 +1,41 @@
 const eventsService = require('./events.service');
-const {
-  HTTP_NOT_FOUND,
-  HTTP_CREATED,
-  HTTP_OK,
-} = require('../config/constants');
+const { HTTP_NOT_FOUND, HTTP_CREATED, HTTP_OK } = require('../config/constants');
 
-module.exports.createEvent = async (req, res, next) => {
-  try {
-    const event = await eventsService.createEvent(req.body);
-    return res.status(HTTP_CREATED.code).json({ data: event });
-  } catch (err) {
-    console.error('Controller createEvent err', err);
-    return next(new Error(err));
-  }
+module.exports.createEvent = async (req, res) => {
+  const event = await eventsService.createEvent(req.body);
+  return res.status(HTTP_CREATED.code).json({ data: event });
 };
 
-module.exports.getEvents = async (req, res, next) => {
-  try {
-    const { pageNumber, pageSize } = req.query;
-    const [events, totalEvents] = await Promise.allSettled([
-      eventsService.getEvents(pageNumber, pageSize),
-      eventsService.getEventCount(),
-    ]);
+module.exports.getEvents = async (req, res) => {
+  const { pageNumber, pageSize } = req.query;
+  const [events, totalEvents] = await Promise.allSettled([
+    eventsService.getEvents(pageNumber, pageSize),
+    eventsService.getEventCount(),
+  ]);
 
-    return res
-      .status(HTTP_OK.code)
-      .json({ pageNumber, pageSize, totalEvents: totalEvents.value, data: events.value });
-  } catch (err) {
-    console.error('Controller getEvents err', err);
-    return next(new Error(err));
-  }
+  return res
+    .status(HTTP_OK.code)
+    .json({ pageNumber, pageSize, totalEvents: totalEvents.value, data: events.value });
 };
 
-module.exports.getEventById = async (req, res, next) => {
-  try {
-    const event = await eventsService.getEventById(req.params.id);
-    if (!event) return res.status(HTTP_NOT_FOUND).json({ error: HTTP_NOT_FOUND.message });
+module.exports.getEventById = async (req, res) => {
+  const event = await eventsService.getEventById(req.params.id);
+  if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
 
-    return res.status(HTTP_OK.code).json({ data: event });
-  } catch (err) {
-    console.error('Controller getEventById err', err);
-    return next(new Error(err));
-  }
+  return res.status(HTTP_OK.code).json({ data: event });
 };
 
-module.exports.updateEventById = async (req, res, next) => {
-  try {
-    const event = await eventsService.updateEventById(req.params.id, req.body);
-    if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
-    const updatedEvent = await eventsService.getEventById(req.params.id);
+module.exports.updateEventById = async (req, res) => {
+  const event = await eventsService.updateEventById(req.params.id, req.body);
+  if (!event[0]) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
+  const updatedEvent = await eventsService.getEventById(req.params.id);
 
-    return res.status(HTTP_OK.code).json({ data: updatedEvent });
-  } catch (err) {
-    console.error('Controller updateEventById err', err);
-    return next(new Error(err));
-  }
+  return res.status(HTTP_OK.code).json({ data: updatedEvent });
 };
 
-module.exports.deleteEventById = async (req, res, next) => {
-  try {
-    const event = await eventsService.deleteEventById(req.params.id);
-    if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
+module.exports.deleteEventById = async (req, res) => {
+  const event = await eventsService.deleteEventById(req.params.id);
+  if (!event) return res.status(HTTP_NOT_FOUND.code).json({ error: HTTP_NOT_FOUND.message });
 
-    return res.status(HTTP_OK.code).json({ data: !!event });
-  } catch (err) {
-    console.error('Controller deleteEventById err', err);
-    return next(new Error(err));
-  }
+  return res.status(HTTP_OK.code).json({ data: !!event });
 };
